581e2287b638742ee312c18e3f6690d019927ec1
eduardo3g <eduardosbrasil10@gmail.com>
2020-05-31 22:55:00 -0300

    chore: Placing a bid to an auction.

---
+++ src/handlers/placeBid.js
@@ -0,0 +1,37 @@
+import AWS from 'aws-sdk';
+import commomMiddleware from '../lib/commomMiddleware';
+import createError from 'http-errors';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function placeBid(event, context) {
+  const { id } = event.pathParameters;
+  const { amount } = event.body;
+
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Key: { id },
+    UpdateExpression: 'set highestBid.amount = :amount',
+    ExpressionAttributeValues: {
+      ':amount': amount,
+    },
+    ReturnValues: 'ALL_NEW',
+  };
+
+  let updatedAuction;
+
+  try {
+    const result = await dynamodb.update(params).promise();
+
+    updatedAuction = result.Attributes;
+  } catch (error) {
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(updatedAuction),
+  };
+}
+
+export const handler = commomMiddleware(placeBid);
