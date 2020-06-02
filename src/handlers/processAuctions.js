7ee6f77ee83b5148419713094c61fe54d87b09b2
eduardo3g <eduardosbrasil10@gmail.com>
2020-06-01 22:50:54 -0300

    feat: Closing auctions.

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,9 +1,19 @@
+import createError from 'http-errors';
 import { getEndedAuctions } from '../lib/getEndedAuctions';
+import { closeAuction } from '../lib/closeAuction';
 
 async function processAuctions(event, context) {
-  const auctionsToClose = await getEndedAuctions();
+  try {
+    const auctionsToClose = await getEndedAuctions();
+    const closePromises = auctionsToClose.map(auction => closeAuction(auction));
 
-  console.log(auctionsToClose);
+    await Promise.all(closePromises);
+
+    return { closed: closePromises.length };
+  } catch (error) {
+    console.log(error);
+    throw new createError.InternalServerError(error);
+  }
 }
 
 export const handler = processAuctions;
