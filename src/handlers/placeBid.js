d86f089f283e6506f10fe5a89bf489adce6bb798
eduardo3g <eduardosbrasil10@gmail.com>
2020-06-08 19:56:34 -0300

    chore: Setting bidder e-mail address on placeBid function.

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -11,6 +11,7 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function placeBid(event, context) {
   const { id } = event.pathParameters;
   const { amount } = event.body;
+  const { email } = event.requestContext.authorizer;
 
   const auction = await getAuctionById(id);
 
@@ -25,9 +26,10 @@ async function placeBid(event, context) {
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: { id },
-    UpdateExpression: 'set highestBid.amount = :amount',
+    UpdateExpression: 'set highestBid.amount = :amount, highestBid.bidder =:bidder',
     ExpressionAttributeValues: {
       ':amount': amount,
+      ':bidder': email,
     },
     ReturnValues: 'ALL_NEW',
   };
