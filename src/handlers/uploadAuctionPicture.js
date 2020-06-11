400f4f417e2e1ebef86c84b482162960d07c5f97
eduardo3g <eduardosbrasil10@gmail.com>
2020-06-10 21:35:55 -0300

    Added cors configurations (still disabled).

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,6 +1,7 @@
 import middy from '@middy/core';
 import httpErrorHandler from '@middy/http-error-handler';
 import validator from '@middy/validator';
+import cors from '@middy/http-cors';
 import createError from 'http-errors';
 import { getAuctionById } from './getAuction';
 import { uploadPictureToS3 } from '../lib/uploadPictureToS3';
@@ -37,4 +38,5 @@ export async function uploadAuctionPicture(event) {
 
 export const handler = middy(uploadAuctionPicture)
   .use(httpErrorHandler())
-  .use(validator({ inputSchema: uploadAuctionPictureSchema }));
+  .use(validator({ inputSchema: uploadAuctionPictureSchema }))
+  .use(cors());
